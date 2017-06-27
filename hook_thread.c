03e459f30652be6235ea76ccfee6a6908ce1d43a
unknown <kevoreilly@gmail.com>
2017-06-27 12:16:40 +0000

    Fix 'instruction pointer' log value in Get/SetContextThread hooks.

--- hook_thread.c
+++ hook_thread.c
@@ -233,9 +233,9 @@ HOOKDEF(NTSTATUS, WINAPI, NtGetContextThread,
     NTSTATUS ret = Old_NtGetContextThread(ThreadHandle, Context);
 	if (Context->ContextFlags & CONTEXT_CONTROL)
 #ifdef _WIN64
-		LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Rip);
+		LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Rcx);
 #else
-		LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Eip);
+		LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Eax);
 #endif
 	else
 		LOQ_ntstatus("threading", "p", "ThreadHandle", ThreadHandle);
@@ -262,12 +262,12 @@ HOOKDEF(NTSTATUS, WINAPI, NtSetContextThread,
         if (CurrentInjectionInfo && CurrentInjectionInfo->ProcessId == pid)
             CurrentInjectionInfo->EntryPoint = Context->Rcx - CurrentInjectionInfo->ImageBase;  // rcx holds rip on 64-bit
 		
-        LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Rip);
+        LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Rcx);
 #else
         if (CurrentInjectionInfo && CurrentInjectionInfo->ProcessId == pid)
             CurrentInjectionInfo->EntryPoint = Context->Eax - CurrentInjectionInfo->ImageBase;  // eax holds eip on 32-bit
 		
-        LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Eip);
+        LOQ_ntstatus("threading", "pp", "ThreadHandle", ThreadHandle, "InstructionPointer", Context->Eax);
 #endif
         DoOutputDebugString("NtSetContextThread hook: Hollow process entry point reset via NtSetContextThread to 0x%x (process %d).\n", CurrentInjectionInfo->EntryPoint, pid);
     }
