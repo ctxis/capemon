450de5984bb24fe2298ada811f5462f5826f28e8
unknown <kevoreilly@gmail.com>
2018-08-07 12:11:17 +0100

    Remove call to ProcessTrackedRegion from terminate_event_thread

--- unhook.c
+++ unhook.c
@@ -28,8 +28,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define UNHOOK_MAXCOUNT 2048
 #define UNHOOK_BUFSIZE 32
 
-extern void ProcessTrackedRegion(void);
-
 static HANDLE g_unhook_thread_handle, g_watcher_thread_handle;
 
 // Index for adding new hooks and iterating all existing hooks.
@@ -253,8 +251,7 @@ static DWORD WINAPI _terminate_event_thread(LPVOID param)
 
 	while (1) {
 		WaitForSingleObject(g_terminate_event_handle, INFINITE);
-        ProcessTrackedRegion();
- 		log_flush();
+		log_flush();
 	}
 
 	return 0;
